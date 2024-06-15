ab4eaddaa98f74e23cbdd4895c73d9481db5e1de
mayromyller <deehmyller11@gmail.com>
2024-06-14 23:32:13 -0300

    feat: Add StripeSubscriptionCreationButton component

--- src/app/components/SubmitButton.tsx
+++ src/app/components/SubmitButton.tsx
@@ -21,3 +21,22 @@ export function SubmitButton() {
     </>
   );
 }
+
+export function StripeSubscriptionCreationButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-full">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+          Enviando...
+        </Button>
+      ) : (
+        <Button type="submit" className="w-full">
+          Assinar
+        </Button>
+      )}
+    </>
+  );
+}

2ad6f23c305da2bdaaa2ce303b2373591c1a6d02
mayromyller <deehmyller11@gmail.com>
2024-06-16 20:43:53 -0300

    docs: Add documentation for server component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,7 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          {/* @ts-expect-error Server Component */}
           <Navbar />
           {children}
         </ThemeProvider>
